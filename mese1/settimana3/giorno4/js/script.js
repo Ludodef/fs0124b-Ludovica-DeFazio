9e1b9c626a2273dd25a28c277b07a4926d8a19a3
Ludodef <defazioludovica95@gmai.com>
2024-02-08 16:55:33 +0100

    esercizio parte2

--- mese1/settimana3/giorno4/js/script.js
+++ mese1/settimana3/giorno4/js/script.js
@@ -1,7 +1,5 @@
 const tableTombolaElement = document.querySelector('.table-tombola');
-const buttonTombola = document.querySelector('.extract').addEventListener('click', ()=>{
-    random()
-})
+const buttonTombola = document.querySelector('.extract')
 
 
 for(let i =1; i <= 99;i++){
@@ -10,9 +8,13 @@ tombolaBox.classList.add('tabella')
 tombolaBox.innerText = i 
 
 tableTombolaElement.append(tombolaBox)
+buttonTombola.addEventListener('click', ()=>{
+    random()
+random()})
 }
 
 function random(){
         let numeroCasuale = Math.floor(Math.random()* 100)
-
+        document.querySelector('.number').innerHTML= numeroCasuale
     }
+
