3030997a32e94007f811cfd5c4661f8d6e1dfaee
Ludodef <defazioludovica95@gmai.com>
2024-03-01 15:55:03 +0100

    verifica settimanale con footer da finire

---
+++ mese2/settimana2/verifica/js/splide.js
@@ -0,0 +1,59 @@
+var splide = new Splide( '.splide', {
+    perPage: 6,
+    gap    : '2rem',
+    autoWidth: true,
+    breakpoints: {
+      640: {
+        perPage: 2,
+        gap    : '.7rem',
+        
+      },
+      480: {
+        perPage: 1,
+        gap    : '.7rem',
+        
+      },
+    },
+  } );
+  
+  splide.mount();
+  
+  var splide = new Splide( '.splide2', {
+    perPage: 6,
+    gap    : '2rem',
+    autoWidth: true,
+    breakpoints: {
+      640: {
+        perPage: 2,
+        gap    : '.7rem',
+        
+      },
+      480: {
+        perPage: 1,
+        gap    : '.7rem',
+        
+      },
+    },
+  } );
+  
+  splide.mount();
+
+  var splide = new Splide( '.splide3', {
+    perPage: 6,
+    gap    : '2rem',
+    autoWidth: true,
+    breakpoints: {
+      640: {
+        perPage: 2,
+        gap    : '.7rem',
+        
+      },
+      480: {
+        perPage: 1,
+        gap    : '.7rem',
+        
+      },
+    },
+  } );
+  
+  splide.mount();
