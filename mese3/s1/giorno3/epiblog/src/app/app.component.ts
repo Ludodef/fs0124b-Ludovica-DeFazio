ccf2a03964068217be84338ed931cec2a7c3e774
Ludodef <defazioludovica95@gmai.com>
2024-03-20 16:42:11 +0100

    esercizio angular articoli random no

--- mese3/s1/giorno3/epiblog/src/app/app.component.ts
+++ mese3/s1/giorno3/epiblog/src/app/app.component.ts
@@ -9,9 +9,27 @@ import { Articles } from './models/articles';
 export class AppComponent {
   articleArr:Articles[] = [];
 
+
+
+
   ngOnInit(){
     fetch('../assets/db.json')
     .then(article => article.json())
-    .then(article => this.articleArr = article.posts)
+    .then(article => this.articleArr = article.posts,)
+
+  }
+
+  generateRandomNumber(){
+    let index:number = Math.floor(Math.random() + this.articleArr.length)
+    return index
+  }
+
+  randomArray:Articles[] = []
+  generateRandomArray(){
+  for (let i = 0 ; i < 4; i++){
+    this.randomArray.push(this.articleArr[this.generateRandomNumber()])
+  }
+
+
   }
 }
