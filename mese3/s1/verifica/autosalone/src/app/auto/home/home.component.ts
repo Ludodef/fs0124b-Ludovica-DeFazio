448deaaff7b0e63dded3fa0be6be935cbebf01cb
Ludodef <defazioludovica95@gmai.com>
2024-03-22 13:06:36 +0100

    verifica settimanale autosalone 2

--- mese3/s1/verifica/autosalone/src/app/auto/home/home.component.ts
+++ mese3/s1/verifica/autosalone/src/app/auto/home/home.component.ts
@@ -9,11 +9,31 @@ import { AutoService } from '../../auto.service';
 })
 export class HomeComponent {
 autoArr:Car[] = []
+autoRandom:Car [] = []
+
 
 constructor (private carSvc:AutoService){}
 
 ngOnInit(){
 
-this.carSvc.getAllCar().then(res => this.autoArr = res)
+
+this.carSvc.getAllCar().then(res => {
+  this.autoArr = res;
+  this.getRandomCar(2)
+  })
+
+}
+
+getRandomCar(n:number){
+
+  for(let i = 0; i < n; i++){
+
+    let rand = Math.floor(Math.random() * this.autoArr.length)
+
+    this.autoRandom.push( this.autoArr[rand] );
+
+  }
+console.log(this.autoRandom);
+
 }
 }
