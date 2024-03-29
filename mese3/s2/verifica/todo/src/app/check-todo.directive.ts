1c13a05c31c2b40beda83a113974b90be198352e
Ludodef <defazioludovica95@gmai.com>
2024-03-29 17:13:04 +0100

    todo direttive eliminato

--- mese3/s2/verifica/todo/src/app/check-todo.directive.ts
+++ mese3/s2/verifica/todo/src/app/check-todo.directive.ts
@@ -4,11 +4,5 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appCheckTodo]'
 })
 export class CheckTodoDirective {
-  @Input() appCheckTodo: any
-
-  constructor(private ref:ElementRef) { }
-  @HostListener('change')onChecked(checked:boolean){
-    this.appCheckTodo.completed = checked
-  }
 
 }
