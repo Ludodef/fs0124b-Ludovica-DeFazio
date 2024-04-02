98ab828c18092b8fc9c52cbe1c39c7e7dc4035e3
Ludodef <defazioludovica95@gmai.com>
2024-04-02 15:44:15 +0200

    esercizio moduli

---
+++ mese3/s4/giorno1/moduli/src/app/pages/chi-siamo/chi-siamo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChiSiamoComponent } from './chi-siamo.component';
+
+
+
+@NgModule({
+  declarations: [
+    ChiSiamoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChiSiamoModule { }
