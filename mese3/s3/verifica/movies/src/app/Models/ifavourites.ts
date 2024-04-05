662a880e2108e417a644631f5ae3e384955ab342
Ludodef <defazioludovica95@gmai.com>
2024-04-05 16:02:11 +0200

    scheletro esercizio

---
+++ mese3/s3/verifica/movies/src/app/Models/ifavourites.ts
@@ -0,0 +1,2 @@
+export interface Ifavourites {
+}
--- mese3/s3/verifica/movies/src/app/app-routing.module.ts
+++ mese3/s3/verifica/movies/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', loadChildren: () => import('./page/home/home.module').then(m => m.HomeModule) }, { path: 'utenti', loadChildren: () => import('./page/utenti/utenti.module').then(m => m.UtentiModule) }, { path: 'profilo', loadChildren: () => import('./page/profilo/profilo.module').then(m => m.ProfiloModule) }, { path: 'auth', loadChildren: () => import('./page/auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
